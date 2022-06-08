123d379e7200a20f0b72e5c9e8755cf1c3e8218f
Thomas Barry <thomas.barry@schlesingergroup.com>
2022-06-08 14:13:30 -0500

    first commit

---
+++ src/Dummy.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDummy = styled.div`
+  color: red;
+`;
+
+const Dummy = () => {
+  return <StyledDummy>FOO</StyledDummy>;
+};
+
+export default Dummy;
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import './index.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
