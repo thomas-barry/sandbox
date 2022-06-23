a6755a9cb702abf031808abb93610d117d9bbcea
Thomas Barry <thomas@curmudgeon.local>
2022-06-23 07:37:23 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from 'react';
-import Container from 'react-bootstrap/Container';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
-import ModalExample from './ModalExample';
-import PopoverExample from './PopoverExample';
+import Page from '@/layout/page/Page';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 12px;
+    font-family: "Roboto, Arial, sans-serif";
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    overflow-y: scroll;
+    background-color: lightgray; 
+  }
+`;
 
 const App = () => {
-  useEffect(() => {
-    const array = [9, 4, 3, 7, 1, 8, 6, 2];
-    const sorted = array.slice().sort();
-    for (let i = 0; i < sorted.length - 1; i++) {
-      if (sorted[i] + 1 !== sorted[i + 1]) {
-        console.log('Missing number:', sorted[i + 1]);
-        break;
-      }
-    }
-  }, []);
-
   return (
     <div id="app-container">
-      <Container className="p-3">
-        <PopoverExample />
-        <ModalExample />
-      </Container>
+      <GlobalStyle />
+      <Page>FOO</Page>
     </div>
   );
 };
